9692a42e2275e3ad1cd022918f4f63919f1dbba0
user-1593 <102684127+user-1593@users.noreply.github.com>
2022-04-25 16:24:50 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -237,6 +237,9 @@ def is_gdrive_link(url: str):
 def is_gdtot_link(url: str):
     url = match(r'https?://.+\.gdtot\.\S+', url)
     return bool(url)
+def is_appdrive_link(url: str):
+    url = match(r'https?://(?:\S*\.)?(?:appdrive|driveapp)\.in/\S+', url)
+    return bool(url)
 
 def is_mega_link(url: str):
     return "mega.nz" in url or "mega.co.nz" in url
